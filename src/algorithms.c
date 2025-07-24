a76352d29f3cc6a8df678cf3078faae00cc65aa8
Gabriel Araujo <gabrielg.araujo@hotmail.com>
2025-07-24 18:39:08 -0300

    Move algorithm's helper functions to it's own file

--- src/algorithms.c
+++ src/algorithms.c
@@ -127,32 +127,3 @@ void	sort_large(t_stack *stack_a, t_stack *stack_b)
     // Phase 3: Merge sorted stacks back together
     merge_sorted_stacks(stack_a, stack_b);
 }
-
-/* Split stack A into two halves - smaller values to B, larger to A */
-void	split_stack_optimally(t_stack *stack_a, t_stack *stack_b)
-{
-    int	median;
-    int	pushed_count;
-    int	target_size;
-    int	rotations;
-    
-    median = find_median(stack_a);
-    target_size = stack_a->size / 2;
-    pushed_count = 0;
-    rotations = 0;
-    
-    while (pushed_count < target_size && rotations < stack_a->size * 2)
-    {
-        if (stack_a->data[0] <= median)
-        {
-            pb(stack_a, stack_b);
-            pushed_count++;
-            rotations = 0;  // Reset rotation counter
-        }
-        else
-        {
-            ra(stack_a);
-            rotations++;
-        }
-    }
-}
