214c56cc89eb1ad0d9ea91fdd2af2bc065df3405
Gabriel Araujo <gabrielg.araujo@hotmail.com>
2025-07-24 18:42:52 -0300

    Implement simultaneous sorting making use of dual operations when possible

--- src/algorithm_utils.c
+++ src/algorithm_utils.c
@@ -28,3 +28,50 @@ void	split_stack_optimally(t_stack *stack_a, t_stack *stack_b)
         }
     }
 }
+
+/* Sort both stacks simultaneously using dual operations when possible */
+void	dual_bubble_sort(t_stack *stack_a, t_stack *stack_b)
+{
+    int	swapped;
+    int	pass;
+    
+    // Sort stack A (ascending) and stack B (descending) simultaneously
+    pass = 0;
+    while (pass < stack_a->size + stack_b->size)
+    {
+        swapped = 0;
+        
+        // Try dual rotation first
+        if (can_dual_rotate(stack_a, stack_b))
+        {
+            rr(stack_a, stack_b);
+            swapped = 1;
+        }
+        // Try dual swap
+        else if (can_dual_swap(stack_a, stack_b))
+        {
+            ss(stack_a, stack_b);
+            swapped = 1;
+        }
+        // Try dual reverse rotation
+        else if (can_dual_reverse_rotate(stack_a, stack_b))
+        {
+            rrr(stack_a, stack_b);
+            swapped = 1;
+        }
+        // Individual operations
+        else
+        {
+            swapped += single_stack_operations(stack_a, stack_b);
+        }
+        
+        // If both stacks are sorted, break
+        if (is_sorted_ascending(stack_a) && is_sorted_descending(stack_b))
+            break;
+        
+        if (!swapped)
+            break;
+        
+        pass++;
+    }
+}
